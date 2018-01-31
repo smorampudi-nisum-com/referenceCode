f0b29e03df7337bbc57170c90cb6d203e49b3afc
santoshmai <30416641+smorampudi-nisum-com@users.noreply.github.com>
2018-01-31 11:56:00 -0800

    ang_cli

---
+++ ang_cli/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import * from 'ts-comment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] 
+   /*styles:[`
+  			h3 {
+  			color:darkblue;
+  			}
+  `]*/
+})
+export class AppComponent {
+  /* title = 'app works'; 
+  name = ''; */
+}
